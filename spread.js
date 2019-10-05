1f7a9726bb02ae0b49f8db8174c63f662244893e
nhloder <nhloder@gmail.com>
2019-10-04 18:41:42 -0600

    ntdun1

--- spread.js
+++ spread.js
@@ -17,8 +17,7 @@ let product = {
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg'
 }
 
-// code here
-
+let productCopy = {...product}
 
 
 
@@ -28,7 +27,7 @@ let product = {
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+let colorsCopy = [...colors];
 
 
 
@@ -43,7 +42,7 @@ function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// code here
+const result = addNums(...numbers)
 
 
 ////////// PROBLEM 4 //////////
@@ -54,3 +53,4 @@ let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
 // code here
+const combinedArrays = arr1.concat(arr2)
